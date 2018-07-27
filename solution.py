164a0b4fdfc7b937b3c027120a9225de8e54a0e6
gasan <abduragimov.ga@gmail.com>
2018-07-27 14:21:00 +0500

    1

---
+++ solution.py
@@ -0,0 +1,11 @@
+x, y = float(input()), int(input())
+i = 1
+if x != y:
+    while x <= y:
+        x = x + x * 0.1
+        i += 1
+    print(i)
+elif x == y == 0:
+    print(0)
+else:
+    print(i)
---
+++ solution1.py
@@ -0,0 +1,7 @@
+x = int(input())
+y = x
+while x != 0:
+    if x > y:
+        y = x
+    x = int(input())
+print(y)
